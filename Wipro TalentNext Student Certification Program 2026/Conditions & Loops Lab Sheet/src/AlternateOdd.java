c8785de315c9cdd759960d4e79f9df216f9a6bed
suchir-42 <suchirreddymuthyala@gmail.com>
2026-07-24 20:36:03 +0530

    Add files via upload

---
+++ Wipro TalentNext Student Certification Program 2026/Conditions & Loops Lab Sheet/src/AlternateOdd.java
@@ -0,0 +1,8 @@
+
+public class AlternateOdd {
+	public static void main(String[] args) {
+		for(int i=1;i<=99;i+=2) {
+			System.out.print(i+" ");
+		}
+	}
+}
---
+++ Wipro TalentNext Student Certification Program 2026/Conditions & Loops Lab Sheet/src/SumEvenOdd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+public class SumEvenOdd { 
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in); 
+		int a = sc.nextInt(); 
+		int b = sc.nextInt(); 
+		int sum = a + b; 
+		if (sum % 2 == 0) 
+			System.out.println("Even"); 
+		else 
+			System.out.println("Odd"); 
+	} 
+} 
